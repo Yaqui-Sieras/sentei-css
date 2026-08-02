b47ff71557dd006e53e883c781b9eb1c8b4c76bb
Yaqueline Maialy <sieras.yaqui94@gmail.com>
2026-08-02 20:31:14 -0300

    Fix: Cambiar site an base en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // GitHub Actions define automáticamente la variable GITHUB_ACTIONS
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 
 // https://astro.build/config
 export default defineConfig({
   // URL completa de tu sitio de GitHub Pages o el que uses
-  site: isGitHubActions ? 'https://Yaqui-Sieras.github.io' : 'https://miResetCSS.vercel.app',
+  site: isGitHubActions
+    ? "https://Yaqui-Sieras.github.io"
+    : "https://sentei-css.vercel.app",
 
   // Nombre del repositorio (solo si NO es un repositorio de tipo usuario/organización principal)
   // Si se está compilando en GitHub Actions usa la subcarpeta,
   // si es en Vercel, Netlify o en local (npm run dev), usa la raíz '/'
-  base: isGitHubActions ? '/miResetCSS/' : '/',
+  base: isGitHubActions ? "/sentei-css/" : "/",
 
   integrations: [sitemap()],
 });
